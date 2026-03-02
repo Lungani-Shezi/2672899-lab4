227d8f6378764751abb58506079599ed19f02f05
Lunagani <dyingmountain58@gmail.com>
2026-03-02 15:25:22 +0200

    third commit

--- script.js
+++ script.js
@@ -4,18 +4,18 @@ const img = document.getElementById("flag");
 const fetchCountry = async () => {
 
     try{
-        const search = document.getElementById("country-input").ariaValueMax.toLowerCase();
+        const search = document.getElementById("country-input").value.toLowerCase();
         const url = `https://restcountries.com/v3.1/name/${search}`;
-        const response = await fectch(url);
+        const response = await fetch(url);
 
         if (!response.ok){
-            throw new Error("Can\'t fetch data");
+            throw new Error("Cant fetch data");
         }
 
         const data = await response.json();
         console.log(data);
     }
-    catch{
+    catch(error){
         console.error(error);
     }
 }
